12b8950f2684606c907fdbaf7e65606c76ca1519
Matt <39469614+KosmicShovel@users.noreply.github.com>
2021-05-27 09:33:27 -0500

    remade it
    no more stdlib or string headers
    using printf, puts, and scanf_s instead of cout or cin
    made my own random number generator

--- rubiks scrambler.cpp
+++ rubiks scrambler.cpp
@@ -1,50 +1,61 @@
 #include <iostream>
-#include <stdlib.h>
 #include <time.h>
-#include <string>
-using namespace std;
 
-int main() 
-{
-    srand (time (NULL));
-    const char moves[6] = {'U', 'F', 'B', 'D', 'R', 'L'};
-    const string invert[2] ={"","' "};
+typedef unsigned long long int ulong;
+ulong lastRand;
+
+ulong getRand() {
+    ulong key[] = { lastRand/2, lastRand/3, lastRand/4, lastRand/5, lastRand/6, lastRand/7, lastRand/8, lastRand/9, lastRand/10 };
+    ulong v = lastRand & 0xFFFFFFFFFFFFul;
+    ulong v0 = (v >> 24) & 0xFFFFFFu;
+    ulong v1 = (v & 0xFFFFFFu);
+    ulong sum = 0u;
+    for (int i = 0; i < 32; ++i)
+    {
+        v0 += (((v1 << 3) ^ (v1 >> 4)) + v1) ^ (sum + key[sum & 3]);
+        v0 &= 0xFFFFFFu;
+        sum += 0xF1BBC; // floor((sqrt(5) - 2) * pow(2, 22))
+        sum &= 0xFFFFFFu;
+        v1 += (((v0 << 3) ^ (v0 >> 4)) + v0) ^ (sum + key[(sum >> 8) & 3]);
+        v1 &= 0xFFFFFFu;
+    }
+    return lastRand = ((ulong)v0 << 24) + v1;
+}
+
+int main() {
+    lastRand = time(NULL);
+
+    const char moves[6] = { 'U', 'F', 'B', 'D', 'R', 'L' };
+    const char* invert[2] = { "", "'" };
     int randindex;
-    int previndex;
+    int previndex{};
     int invertindex;
     int move2;
-    string rotate2;
+    const char* rotate2;
     int move_number;
     int scramblecount;
     int tot_scrambles = 0;
+    puts("How many scrambles would you like: ");
+    scanf_s("%i", &scramblecount);
 
-    cout <<"How many scrambles would you like: ";
-    cin >> scramblecount;
-
-    while (tot_scrambles++ < scramblecount)
-    {
+    while (tot_scrambles++ < scramblecount) {
         move_number = 0;
-        while (move_number++ < 20) 
-        {
-            randindex = rand() % 6;
-            invertindex = rand() % 2; 
+        while (move_number++ < 20) {
+            randindex = getRand() % 6;
+            invertindex = getRand() % 2;
             while (randindex == previndex)
-            {
-                randindex = rand() % 6;
-            }
-            if (invertindex == 0)
-            {
-                move2 = rand() % 2;
-                if (move2 == 1) 
-                {
+                randindex = getRand() % 6;
+            if (invertindex == 0) {
+                move2 = getRand() % 2;
+                if (move2 == 1)
                     rotate2 = "2 ";
-                }
-                else rotate2 = " "; 
+                else rotate2 = " ";
             }
-            else { rotate2 = ""; }
-            cout << moves[randindex] << invert[invertindex] << rotate2; 
+            else rotate2 = "";
+
+            printf("%c%s%s", moves[randindex], invert[invertindex], rotate2);
             previndex = randindex;
         }
-        cout << endl;
+        puts("\r\n");
     }
 }
