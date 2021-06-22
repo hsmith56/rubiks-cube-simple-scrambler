b439c9c2713a6eec66bae727dca88f17157cac66
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-06-22 16:31:06 -0400

    Bryson is going for 5 lines

--- python scrambley.py
+++ python scrambley.py
@@ -1,6 +1,6 @@
 import random
-moves, b, times = ["U","F","L","R","B","D"], [''], int(input("How many scrambles would you like: "))
-for i in range(times*20):
-    while (x:=random.choice(moves)) == "".join(x for x in b[len(b)-1] if x.isalpha()): pass
+b =  ['']
+for i in range(int(input("How many scrambles would you like: "))*20):
+    while (x:=random.choice(["U","F","L","R","B","D"])) == "".join(x for x in b[len(b)-1] if x.isalpha()): pass
     b.append(x + ("2" if (y:=random.randint(0,2) == 1) else "") + ("'" if y == 0 else ""))
 print("\n".join([" ".join(b[i+1:i+21]) for i in range(0, len(b)-1, 20)]))
