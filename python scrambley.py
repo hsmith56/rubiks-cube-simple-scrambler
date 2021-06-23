b3372e39eabce2279abf53bffe42728e9733795c
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2021-06-23 09:48:04 -0400

    Bryson comes in clutch

--- python scrambley.py
+++ python scrambley.py
@@ -1,5 +1,4 @@
-b, random =  [''], __import__('random')
-for i in range(int(input("How many scrambles would you like: "))*20):
-    while (x:=random.choice(["U","F","L","R","B","D"])) == "".join(x for x in b[len(b)-1] if x.isalpha()): pass
-    b.append(x + ("2" if (y:=random.randint(0,2) == 1) else "") + ("'" if y == 0 else ""))
+b, random, times =  [''], __import__('random'), int(input("How many scrambles would you like: "))
+for i in range(times*20):
+    while not (x:=random.choice(["U","F","L","R","B","D"])) == "".join(x for x in b[len(b)-1] if x.isalpha()) and len(b) <= 20*times: b.append(x + ("2" if (y:=random.randint(0,2) == 1) else "") + ("'" if y == 0 else ""))
 print("\n".join([" ".join(b[i+1:i+21]) for i in range(0, len(b)-1, 20)]))
